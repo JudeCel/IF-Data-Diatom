d9c68db6c4654edd0a4badea1216568baf557d6a
lilutanya <lilu.tanya@gmail.com>
2014-12-17 15:29:52 +0200

    Fix getUser

--- repositories/user/getUser.js
+++ repositories/user/getUser.js
@@ -1,8 +1,11 @@
 var db = require('if-data').db;
 var Q = require('q');
 
+
 function getUser(params) {
-    var sql = "SELECT * FROM users WHERE id = ?";
+    var sql = "SELECT id, name_first, name_last, gender, email, \
+     address, phone, mobile, state, country_id, city, code, company \
+     FROM users WHERE id = ?";
     return Q.nfcall(db.queryOne, sql, [params.userId]);
 }
 
